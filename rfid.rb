05039b456c22e1c28755d27c6b7c09cae9c168c2
benjab <benjab@benjab-kontor.(none)>
2012-04-14 12:54:12 +0200

    code cleanup

--- rfid.rb
+++ rfid.rb
@@ -3,46 +3,44 @@ require 'serialport'
 require 'yaml'
 
 CONFIG     = YAML::load_file('config/config.yml')
-RFIDCODES  = YAML::load_file(CONFIG['rfidcodes']['file'])
+RFIDCODES  = YAML::load_file(CONFIG['rfidcodes']['config_file'])
 SERIALPORT = CONFIG['serialport']
 
-def start_reader
+def initialize_serialport
   @sp = SerialPort.new(SERIALPORT['usb_port'], SERIALPORT['baud_rate'], SERIALPORT['data_bits'], SERIALPORT['stop_bits'], SerialPort::module_eval("#{SERIALPORT['parity']}"))
   @sp.read_timeout = 100
 end
 
 def initialize_reader
-  initcodes = RFIDCODES['initcodes']
- # send init
- @sp.write initcodes['init']
- @sp.read
- # send set protocol: Register write request.
- @sp.write initcodes['register_write_request']
- @sp.read
- 
- # 0xF0 AGC selection 
- # 0x00 – AGC enable
- # @sp.write initcodes['agc_enable']
- 
- # 0xF1 AM/PM input selection 
- # 0xFF – AM input
- # @sp.write initcodes['am_input']
+  # send init
+  @sp.write RFIDCODES['initcodes']['init']
+  @sp.read
 end
 
+def initialize_request(initcodes)
+  initcodes.each { |k,v| @sp.write v; @sp.read.inspect } 
+end
 
-def rfid_get_word(tag)
- # get response given in brackets
- tag = tag.match(/\[(.+)\]/).to_s
- # remove brackets
- tag.gsub!(/\[|\]/, "")
+def get_response(r)
+  # get response given in brackets
+  r = r.match(/\[(.+)\]/).to_s
+  # remove brackets
+  response = r.gsub(/\[|\]/, "")
+end
+
+def get_tag(r)
+  tag = r.sub(/,../, "")
+end
+
+def get_bytes(r)
  # split every two char to get hex bytes
- ary = tag.split(/(\w{2})/)
+ ary = r.split(/(\w{2})/)
  ary.delete_if {|c| c.empty? }
  # first byte is response code "00"
  ary.shift
  
  ary.collect! { | byte | byte = byte.hex.chr }
- ary.join
+ response = ary.join
 end
 
 def issue_command(protocol, cmd, cmd_length, options={})
@@ -55,24 +53,22 @@ def issue_command(protocol, cmd, cmd_length, options={})
    :flags => one byte
    :command_code => subcommand
    :offset => offset, one byte
-   :bytes > one byte + 1 (00 = 1)
+   :byte_length > one byte + 1 (00 = 1)
 =end
   case protocol
     when "iso15693"
       unless options[:flags] then options[:flags] = "00" end
       unless options[:command_code] then options[:command_code] = "00" end
       unless options[:offset] then options[:offset] = "00" end
-      unless options[:bytes] then options[:bytes] = "00" end
+      unless options[:byte_length] then options[:byte_length] = "00" end
+      
+      @command = "01" + cmd_length.to_s + "000304" + cmd.to_s + options[:flags] + options[:command_code] + options[:offset] + options[:byte_length] + "0000"
     when "iso14443"
-      #
+      # iso14443 
+      @command = "01" + cmd_length.to_s + "000304" + cmd.to_s
   end
-# iso15693
-  command = "01" + cmd_length.to_s + "000304" + cmd.to_s + options[:flags] + options[:command_code] + options[:offset] + options[:bytes] + "0000"
-# iso14443 
-#  command = "01" + cmd_length.to_s + "000304" + cmd.to_s
 
-  #p command
-  @sp.write command.to_s
+  @sp.write @command.to_s
   response = @sp.read
 end
 
@@ -80,44 +76,66 @@ end
   start main process
 =end
 
-  start_reader.inspect
+  initialize_serialport.inspect
   initialize_reader
 
   # 0x18 - Request command
   # params: flags, command code, data
   # command code 0x20 - read single block
   # response: 00 no tag error, 11 11 11 11 tag block data, 32 bits
-  # tip: "%02X" % num - makes binary string out of fixed number
+  # tip: "%02X" % num - makes binary string out of any two digits
 
   # issuing 010C00030418002301020000 - ISO15693
-  @bytes = 0 # actually means 1
-  @offset = 1
-  @length = 5
-  @result = ""
+@tag = ""
+# loops until tag is found
 while true do
-  
-  iso15693_response = issue_command("iso15693", "18", "0C", :command_code => "23", :offset => "%02X" % @offset, :bytes => "%02X" % @bytes)
-  #iso14443A_reqa = issue_command("iso14443", "A0", "09")
-
-  match = rfid_get_word(iso15693_response)
-  unless match.empty? 
-    @result += match
-    @offset += @bytes + 1
-    
-    break if match == "W_OK"
-    break if @offset == @length
-  else
-    sleep 3
-    #puts "no reply"
+  CONFIG['knowntags'].each do | tagname, tagsettings |
+    protocol = tagsettings['protocol']
+  #RFIDCODES['protocols'].each do | protocol, values |
+    case protocol
+    when "14443A"
+#      puts "initializing ..." + protocol 
+#      values['initcodes'].each { |k,v| @sp.write v; @sp.read.inspect } 
+#      @sp.write values['inventory']
+#      puts "response ..." + protocol 
+#      p @sp.read
+    when "iso15693"
+      rfidcodes = RFIDCODES['protocols'][protocol]
+      puts "initializing ... " + protocol 
+      initialize_request(rfidcodes['initcodes'])
+      
+      # read rfid tag uid
+      @sp.write rfidcodes['inventory']
+      res = @sp.read
+      r = get_response(res)
+      tag = get_tag(r)
+      if !tag.empty? and @tag != tag
+        @tag = tag
+        initialize_request(rfidcodes['initcodes'])
+        byte_length = tagsettings['byte_length']
+        offset = tagsettings['start_offset']
+        length = tagsettings['length']
+        @result = ""
+        # read rfid content
+        while true do
+          read = issue_command(protocol, "18", "0C", :command_code => "23", :offset => "%02X" % offset, :byte_length => "%02X" % byte_length)
+          response = get_response(read)
+          if !response.empty? 
+            @result += get_bytes(response)
+            offset += byte_length + 1
+            break if response == "W_OK"
+            break if offset == length
+          end
+        end
+        tagsettings['extract_values'].each do | name, options |
+          p "#{name}: " + @result[options['offset'],options['length']]
+        end
+      else
+        puts "still same tag ..."
+      end 
+    end # end case protocol
   end
+  sleep 3 # sleep before next loop
+end  
 
-#  match = rfid_get_word(iso14443A_reqa)
-end
-strekkode = @result[1,14]
-tittelnummer = @result[6,6]
-puts "strekkode: " + strekkode
-puts "tittelnummer: " + tittelnummer
-# xdotool sends reply to browser
-`xdotool search --classname Navigator windowactivate --sync type --delay 5 --args 1 "#{tittelnummer}" key Return`
-#p @result
 @sp.close  
